6f74078203daf8503cb7c6598ba91954c56600de
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2021-11-07 13:54:06 +0200

    Update card-grid.jsx

--- Components/card-grid/card-grid.jsx
+++ Components/card-grid/card-grid.jsx
@@ -7,58 +7,43 @@ import camera from '../images/video-camera.png'
 import placeholder from '../images/placeholder.png'
 import '../card-grid/card-grid.scss'
 const CardGrid = () => (
-
     <div className='cardGridContainer'>
         <div className="cardGridParent1">
-            <div className="cardGridChild1">
-                <div className="cardChildImgContainer">
-                    <img className='cardChildImg' src={squares} alt="" />
-                </div>
-                <h1 className='cardGridHeading'>Buisness Automation</h1>
-                <p className='cardGridParagraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, delectus.</p>
+            <div className="cardGridChild">
+                <img src={squares} alt="" className="cardGridImage" />
+                <h1 className='cardGridHeading'>Business Automation</h1>
+                <p className="cardGridParagraph">Lorem ipsum dolor sit amet.</p>
             </div>
-            <div className="cardGridChild1">
-                <div className="cardChildImgContainer">
-                    <img className='cardChildImg' src={headphones} alt="" />
-                </div>
+            <div className="cardGridChild">
+                <img src={headphones} alt="" className="cardGridImage" />
                 <h1 className='cardGridHeading'>Realtime Support</h1>
-                <p className='cardGridParagraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, delectus.</p>
+                <p className="cardGridParagraph">Lorem ipsum dolor sit amet.</p>
             </div>
         </div>
         <div className="cardGridParent2">
-            <div className="cardGridChild1">
-                <div className="cardChildImgContainer">
-                    <img className='cardChildImg' src={shoppingCart} alt="" />
-                </div>
-                <h1 className='cardGridHeading'>E-Commerce Support</h1>
-                <p className='cardGridParagraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, delectus.</p>
+            <div className="cardGridChild">
+                <img src={shoppingCart} alt="" className="cardGridImage" />
+                <h1 className='cardGridHeading'>Ecommerce Support</h1>
+                <p className="cardGridParagraph">Lorem ipsum dolor sit amet.</p>
             </div>
-            <div className="cardGridChild1">
-                <div className="cardChildImgContainer">
-                    <img className='cardChildImg' src={placeholder} alt="" />
-                </div>
+            <div className="cardGridChild">
+                <img src={placeholder} alt="" className="cardGridImage" />
                 <h1 className='cardGridHeading'>Location Marking</h1>
-                <p className='cardGridParagraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, delectus.</p>
+                <p className="cardGridParagraph">Lorem ipsum dolor sit amet.</p>
             </div>
         </div>
         <div className="cardGridParent3">
-            <div className="cardGridChild1">
-                <div className="cardChildImgContainer">
-                    <img className='cardChildImg' src={camera} alt="" />
-                </div>
+            <div className="cardGridChild">
+                <img src={camera} alt="" className="cardGridImage" />
                 <h1 className='cardGridHeading'>Video Production</h1>
-                <p className='cardGridParagraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, delectus.</p>
+                <p className="cardGridParagraph">Lorem ipsum dolor sit amet.</p>
             </div>
-            <div className="cardGridChild1">
-                <div className="cardChildImgContainer">
-                    <img className='cardChildImg' src={upload} alt="" />
-                </div>
+            <div className="cardGridChild">
+                <img src={upload} alt="" className="cardGridImage" />
                 <h1 className='cardGridHeading'>File Upload Protocol</h1>
-                <p className='cardGridParagraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, delectus.</p>
+                <p className="cardGridParagraph">Lorem ipsum dolor sit amet.</p>
             </div>
         </div>
     </div>
-
-
 )
 export default CardGrid;
